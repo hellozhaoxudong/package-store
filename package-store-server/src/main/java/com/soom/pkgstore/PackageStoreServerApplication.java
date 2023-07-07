ef40d23a2fbf17be10912fef5c8242a024e596e8
zhaoxudong <zhaoxudong@crhms.cn>
2023-07-07 17:54:02 +0800

    first commit

---
+++ package-store-server/src/main/java/com/soom/pkgstore/PackageStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.soom.pkgstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageStoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageStoreServerApplication.class, args);
+    }
+
+}
---
+++ package-store-server/src/test/java/com/soom/pkgstore/PackageStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soom.pkgstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageStoreServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
